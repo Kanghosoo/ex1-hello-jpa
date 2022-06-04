cb86dab426ec7f300a734a999b9520bfc837e35c
HOSOO KANG <hosoo3513@gmail.com>
2022-06-04 14:48:11 +0900

    CASCADE

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,25 +15,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            em.flush();
-            em.clear();
-
-            Member m = em.find(Member.class, member1.getId());
-
-            System.out.println("m = " + m.getTeam().getClass());
-
-            System.out.println("=========");
-            m.getTeam().getName(); //초기화
-            System.out.println("=========");
+            em.persist(parent);
 
             tx.commit();
         } catch (Exception e) {
